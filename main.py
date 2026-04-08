2fcde3fab09b3472b613cbbe9c4249114ade5550
Ali Raza <alirazahaider2@gmail.com>
2026-04-08 14:59:38 +0500

    Implement prescription management API and update dependencies

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 from fastapi import FastAPI
+from schemas import UserInput, Response
+from contextlib import asynccontextmanager
+from database import get_db, AsyncSession, Base, engine
+
+
+@asynccontextmanager
+async def lifespan(_app: FastAPI):
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    yield
+    await engine.dispose()
 
 
 app = FastAPI()
@@ -7,3 +18,8 @@ app = FastAPI()
 @app.get("/")
 def home():
     return "Hello!!"
+
+
+@app.post("/prescription", response_model=Response)
+def create_prescription(prescription: UserInput):
+    pass
--- schemas.py
+++ schemas.py
@@ -1,7 +1,12 @@
-import string
+from datetime import datetime, UTC
 
 from pydantic import Field, BaseModel, ConfigDict
 
 
 class UserInput(BaseModel):
     image_url: str = Field(min_length=1, max_length=200)
+    content: str | None = Field(default=None)
+
+
+class Response(BaseModel):
+    pass
