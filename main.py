a39bd0c87d73e7450b66aea0bd0b69d20dc0673b
Ali Raza <alirazahaider2@gmail.com>
2026-04-15 10:10:13 +0500

    add basic rate limiter for a sensitive route

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-from fastapi import FastAPI, File, Form, Request, UploadFile
+from fastapi import FastAPI, File, Form, Request, UploadFile, HTTPException
 from contextlib import asynccontextmanager
 from database import Base, engine
 from services.imagekit_service import upload_file
 from starlette.exceptions import HTTPException as StarletteHTTPException
 from services.gemini_service import generate_prescription
 from fastapi.responses import JSONResponse
+from time import time
+from starlette.middleware.base import BaseHTTPMiddleware
+
+
+requests_counts = {}
+RATE_LIMIT = 1
+TIME_WINDOW = 60  # Max 1 request per minute
 
 
 @asynccontextmanager
@@ -16,6 +23,33 @@ async def lifespan(_app: FastAPI):
 
 
 app = FastAPI()
+
+
+class RateLimitMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request: Request, call_next):
+        if request.url.path != "/prescription":
+            return await call_next(request)
+        client_ip = request.client.host if request.client else "unknown"
+        current_time = time()
+
+        if client_ip not in requests_counts:
+            requests_counts[client_ip] = []
+
+        requests_counts[client_ip] = [
+            t for t in requests_counts[client_ip] if current_time - t < TIME_WINDOW
+        ]
+
+        if len(requests_counts[client_ip]) >= RATE_LIMIT:
+            return JSONResponse(
+                status_code=429,
+                content={"detail": "Too many requests. Please try again later."},
+            )
+        requests_counts[client_ip].append(current_time)
+        response = await call_next(request)
+        return response
+
+
+app.add_middleware(RateLimitMiddleware)
 
 
 @app.get("/")
